f93979ab0b9892538683f4c65bad5c5befc88ce1
vagrant <vagrant@precise32.(none)>
2014-02-11 01:29:25 +0000

    added last two slides

--- routes/add.js
+++ routes/add.js
@@ -1,7 +1,18 @@
 var data = require("../data.json");
 
 exports.addFriend = function(req, res) {    
+	console.log("sdlfkjs;dlkf");
 	console.log(data);
-	res.render('addFriend', data);
+	name = req.query.name;
+	description=req.query.description;
+	newFriend=	{
+			"name": name,
+			"description": description,
+			"imageURL": "http://lorempixel.com/500/500/people"	
+		};
+
+	data["friends"].push(newFriend);
+	res.render("add", data);
+	console.log(data);
 	// Your code goes here
  }
